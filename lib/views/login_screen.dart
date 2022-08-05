6ad2fd0f2cf00dea0b38776fb2d959c2560c9bfd
Eugene Asiedu <uginasiedu079@gmail.com>
2022-08-05 03:29:42 +0000

    Added login screen(not implemented)

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget{
+
+static const String routeName = "login";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+}
