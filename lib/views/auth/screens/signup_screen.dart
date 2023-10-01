f66db2ded8a228bbdcf9d78c0dd8c3f8f07d9d5f
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-01 00:50:56 +0000

    Redisigned layout

--- lib/views/auth/screens/signup_screen.dart
+++ lib/views/auth/screens/signup_screen.dart
@@ -13,13 +13,15 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  late TextEditingController controller;
+  late TextEditingController phoneController;
+  late TextEditingController passwordController;
 
   String countryCode = '+233';
 
   @override
   void initState() {
-    controller = new TextEditingController();
+    phoneController = new TextEditingController();
+    passwordController = new TextEditingController();
     super.initState();
   }
 
@@ -32,58 +34,44 @@ class _SignupScreenState extends State<SignupScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Container(
               height: height,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  /* IntlPhoneField(
-                    controller: controller,
-                  ), */
-                  Stack(
-                    alignment: AlignmentDirectional.centerStart,
-                    children: [
-                      Expanded(
-                        flex: 20,
-                        child: TextFieldTemplate(
-                          hintText: "Phone",
-                          controller: controller,
-                          obscureText: false,
-                          width: width,
-                          height: 50,
-                          textInputType: TextInputType.phone,
-                          textInputAction: TextInputAction.next,
-                          enabled: true,
-                          leftContentPadding: 120,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 9,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 3.0),
-                          child: CountryCodeLayout(
-                            countryCode: countryCode,
-                          ),
-                        ),
-                      ),
-                    ],
+                  TextFieldTemplate(
+                    hintText: "Phone",
+                    controller: phoneController,
+                    obscureText: false,
+                    width: width,
+                    height: 50,
+                    textInputType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFieldTemplate(
+                    hintText: "Password",
+                    controller: passwordController,
+                    obscureText: false,
+                    width: width,
+                    height: 50,
+                    textInputType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                    enabled: true,
                   ),
                   SizedBox(
                     height: 30.0,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                    child: ButtonTemplate(
-                      buttonName: "Register",
-                      buttonWidth: width,
-                      buttonAction: () {
-                        Navigator.pushNamed(
-                          context,
-                          CreateAccountScreen.routeName,
-                        );
-                      },
-                    ),
+                  ButtonTemplate(
+                    buttonName: "Register",
+                    buttonWidth: width,
+                    buttonAction: () {
+                      Navigator.pushNamed(context, SignupScreen.routeName);
+                    },
                   ),
                 ],
               ),
