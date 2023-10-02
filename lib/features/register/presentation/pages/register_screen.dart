6d5987965eba472fe66efbcdddee76a14f1322f3
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-02 13:14:47 +0000

    Added a welcome message

--- lib/features/register/presentation/pages/register_screen.dart
+++ lib/features/register/presentation/pages/register_screen.dart
@@ -32,65 +32,109 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Container(
-              height: height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFieldTemplate(
-                    hintText: "Phone",
-                    controller: phoneController,
-                    obscureText: false,
-                    width: width,
-                    height: 50,
-                    textInputType: TextInputType.phone,
-                    textInputAction: TextInputAction.next,
-                    enabled: true,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFieldTemplate(
-                    hintText: "Password",
-                    controller: passwordController,
-                    obscureText: false,
-                    width: width,
-                    height: 50,
-                    textInputType: TextInputType.phone,
-                    textInputAction: TextInputAction.next,
-                    enabled: true,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFieldTemplate(
-                    hintText: "Retype password",
-                    controller: retypePasswordController,
-                    obscureText: false,
-                    width: width,
-                    height: 50,
-                    textInputType: TextInputType.phone,
-                    textInputAction: TextInputAction.next,
-                    enabled: true,
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  ButtonTemplate(
-                    buttonName: "Register",
-                    buttonWidth: width,
-                    buttonAction: () {
-                      Navigator.pushNamed(
-                        context,
-                        CreateAccountScreen.routeName,
-                      );
-                    },
-                  ),
-                ],
-              ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 30),
+          child: Container(
+            height: height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Hello! Register to get \nstarted.",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    TextFieldTemplate(
+                      hintText: "Phone",
+                      controller: phoneController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFieldTemplate(
+                      hintText: "Password",
+                      controller: passwordController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFieldTemplate(
+                      hintText: "Retype password",
+                      controller: retypePasswordController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    ButtonTemplate(
+                      buttonName: "Register",
+                      buttonWidth: width,
+                      buttonAction: () {
+                        Navigator.pushNamed(
+                          context,
+                          CreateAccountScreen.routeName,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      " Login",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.orange,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
