92078d674115a067ab8583bef07dc74405fac4cf
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-05 23:59:50 +0000

    Built layout

--- lib/features/change_password/presentation/pages/change_password_screen.dart
+++ lib/features/change_password/presentation/pages/change_password_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rapidlie/core/constants/feature_contants.dart';
+import 'package:rapidlie/core/widgets/button_template.dart';
+import 'package:rapidlie/core/widgets/textfield_template.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({Key? key}) : super(key: key);
@@ -8,10 +11,98 @@ class ChangePasswordScreen extends StatefulWidget {
 }
 
 class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  late TextEditingController oldPasswordController;
+  late TextEditingController newPasswordController;
+  late TextEditingController retypePasswordController;
+
+  @override
+  void initState() {
+    oldPasswordController = TextEditingController();
+    newPasswordController = TextEditingController();
+    retypePasswordController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 30),
+          child: Container(
+            height: height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Hello! Register to get \nstarted.",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    TextFieldTemplate(
+                      hintText: "Old password",
+                      controller: oldPasswordController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    textBoxSpace(),
+                    TextFieldTemplate(
+                      hintText: "New password",
+                      controller: newPasswordController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    textBoxSpace(),
+                    TextFieldTemplate(
+                      hintText: "Retype password",
+                      controller: retypePasswordController,
+                      obscureText: false,
+                      width: width,
+                      height: 50,
+                      textInputType: TextInputType.phone,
+                      textInputAction: TextInputAction.next,
+                      enabled: true,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    ButtonTemplate(
+                      buttonName: "Change password",
+                      buttonWidth: width,
+                      buttonAction: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
