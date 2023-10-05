bd038f771aa422048374081ad62d73adf4bfbad9
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-05 23:54:18 +0000

    Added change password screen

---
+++ lib/features/change_password/presentation/pages/change_password_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswordScreen extends StatefulWidget {
+  const ChangePasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChangePasswordScreen> createState() => _ChangePasswordScreenState();
+}
+
+class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
