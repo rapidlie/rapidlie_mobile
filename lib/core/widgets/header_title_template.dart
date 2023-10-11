6eec0805f74ae43801edd4a6b211daba526d1367
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-11 01:25:45 +0000

    Added header title widget

---
+++ lib/core/widgets/header_title_template.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HeaderTextTemplate extends StatelessWidget {
+  final String titleText;
+  final Color titleTextColor;
+  final Color containerColor;
+  final double textSize;
+
+  const HeaderTextTemplate(
+      {Key? key,
+      required this.titleText,
+      required this.titleTextColor,
+      required this.containerColor,
+      required this.textSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: containerColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Text(
+          titleText,
+          style: TextStyle(
+            fontSize: textSize,
+            fontWeight: FontWeight.w600,
+            fontFamily: "Poppins",
+            color: titleTextColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
