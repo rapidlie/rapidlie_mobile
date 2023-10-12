561d55f04dfa242fa411374c39ea540d4da14bf5
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-12 17:14:09 +0000

    Added two widgets to the events feature

---
+++ lib/features/event/presentation/widgets/explore_categories_list_template.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:rapidlie/core/constants/feature_contants.dart';
+
+class ExploreCategoryListTemplate extends StatelessWidget {
+  final String categoryName;
+
+  const ExploreCategoryListTemplate({Key? key, required this.categoryName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              color: const Color.fromARGB(255, 138, 81, 81),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            categoryName,
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 14.0,
+              color: Color.fromARGB(133, 63, 59, 59),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
