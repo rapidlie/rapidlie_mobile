d3877414628e959d8755589b375abc4c6ac7ef4b
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-12 12:32:46 +0000

    Added border radius constant

--- lib/core/constants/feature_contants.dart
+++ lib/core/constants/feature_contants.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+double width = Get.width;
+double height = Get.height;
+double borderRadius = 8.0;
+
+normalSpacing() {
+  return SizedBox(
+    height: 24,
+  );
+}
+
 textBoxSpace() {
   return SizedBox(
     height: 12,
   );
 }
-
-double width = Get.width;
-double height = Get.height;
