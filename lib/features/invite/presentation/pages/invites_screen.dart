dabbd23684643be377e28758898be0bac8d7e263
Eugene Asiedu <uginasiedu079@gmail.com>
2023-10-26 16:02:03 +0000

    Added invited events listview layout

--- lib/features/invite/presentation/pages/invites_screen.dart
+++ lib/features/invite/presentation/pages/invites_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rapidlie/core/widgets/app_bar_template.dart';
+import 'package:rapidlie/features/home/presentation/widgets/event_list_template.dart';
 
 class InvitesScreen extends StatelessWidget {
   static const String routeName = "invites";
@@ -7,17 +8,49 @@ class InvitesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80),
-          child: AppBarTemplate(
-            pageTitle: "Invites",
-            isSubPage: false,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(80),
+            child: AppBarTemplate(
+              pageTitle: "Invites",
+              isSubPage: false,
+            ),
           ),
-        ),
-        body: Container(
-          child: Center(child: Text("Invites screen")),
-        ),
-      ),
+          body: SingleChildScrollView(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              //controller: _scrollController,
+              physics:
+                  BouncingScrollPhysics(parent: NeverScrollableScrollPhysics()),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: EventListTemplate(
+                    trailingWidget: Row(
+                      children: [
+                        Icon(
+                          Icons.thumb_up,
+                          color: Colors.grey.shade600,
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Icon(
+                          Icons.thumb_down,
+                          color: Colors.grey.shade600,
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )),
     );
   }
 }
