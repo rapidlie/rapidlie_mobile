57b65e7ff4426afa17508ce67486ca6e7dd1656d
Eugene Asiedu <uginasiedu079@gmail.com>
2024-03-29 15:22:30 +0100

    git add function to check default device language

---
+++ lib/core/utils/getDefaultLanguage.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:rapidlie/l10n/app_localizations.dart';
+
+Locale getDefaultDeviceLocale() {
+  // Retrieve the default locale from the window
+  Locale defaultLocale = window.locale;
+  return defaultLocale;
+}
+
+Locale returnLoale() {
+  List<Locale> allLocales = AppLocalizations.supportedLocales;
+  if (allLocales.contains(getDefaultDeviceLocale())) {
+    return getDefaultDeviceLocale();
+  } else {
+    return Locale("en");
+  }
+}
