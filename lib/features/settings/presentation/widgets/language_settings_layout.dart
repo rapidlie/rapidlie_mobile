0b3445c60b1c34f4a681256d6bde65505915408c
Eugene Asiedu <uginasiedu079@gmail.com>
2024-03-29 15:36:25 +0100

    Converted widget as a consumer of ChangeLanguageProvider

--- lib/features/settings/presentation/widgets/language_settings_layout.dart
+++ lib/features/settings/presentation/widgets/language_settings_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rapidlie/core/constants/feature_contants.dart';
+import 'package:rapidlie/features/settings/presentation/providers/change_language_provider.dart';
 import 'package:rapidlie/l10n/app_localizations.dart';
 
 class LanguageSettingsLayout extends StatefulWidget {
@@ -14,48 +16,63 @@ class LanguageSettingsLayout extends StatefulWidget {
 class _LanguageSettingsLayoutState extends State<LanguageSettingsLayout> {
   late List supportedLanguages;
   late var language;
-  int selectedLanguage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     language = AppLocalizations.of(context);
     supportedLanguages = [language.english, language.german, language.french];
-    return Padding(
-      padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 60),
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: supportedLanguages.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedLanguage = index;
-              });
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Container(
-                width: width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      supportedLanguages[index],
-                      style: poppins14black500(),
+    return Consumer<ChangeLanguageProvider>(
+      builder: (context, provider, child) {
+        return Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 60),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: supportedLanguages.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  if (index == 0) {
+                    setState(() {
+                      provider.changeLanguage(Locale("en"));
+                      Navigator.pop(context);
+                    });
+                  } else if (index == 1) {
+                    setState(() {
+                      provider.changeLanguage(Locale("de"));
+                      Navigator.pop(context);
+                    });
+                  } else {
+                    setState(() {
+                      provider.changeLanguage(Locale("fr"));
+                      Navigator.pop(context);
+                    });
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    width: width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          supportedLanguages[index],
+                          style: poppins14black500(),
+                        ),
+                      ],
                     ),
-                    selectedLanguage == index
-                        ? Icon(
-                            Icons.check,
-                            color: Colors.green,
-                          )
-                        : SizedBox()
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
