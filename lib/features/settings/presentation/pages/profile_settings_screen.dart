55ff88eeefad2f82ab19646e6983fd9d0be1ff4c
Eugene Asiedu <uginasiedu079@gmail.com>
2024-04-24 07:55:14 +0200

    Updated profile screen

--- lib/features/settings/presentation/pages/profile_settings_screen.dart
+++ lib/features/settings/presentation/pages/profile_settings_screen.dart
@@ -1,8 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:rapidlie/core/constants/feature_contants.dart';
+import 'package:rapidlie/core/widgets/app_bar_template.dart';
+import 'package:rapidlie/l10n/app_localizations.dart';
+
+import '../widgets/settings_item_layout.dart';
 
 class ProfileSettingsScreen extends StatelessWidget {
+  late var language;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    language = AppLocalizations.of(context);
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: AppBarTemplate(
+          pageTitle: "Profile settings",
+          isSubPage: true,
+        ),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        child: Container(
+          height: height,
+          width: width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Eugene Ofori Asiedu",
+                          style: poppins14black500(),
+                        ),
+                        Text(
+                          "+233 50 613 8718",
+                          style: poppins10black400(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Row(
+                    children: [],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      SettingsItemLayout(
+                        icon: Icons.lock,
+                        title: "Change password",
+                        iconColor: Colors.black,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40.0),
+                        child: Container(
+                          height: 1,
+                          color: const Color.fromARGB(255, 240, 239, 239),
+                        ),
+                      ),
+                      SettingsItemLayout(
+                        icon: Icons.delete,
+                        title: "Delete account",
+                        iconColor: Colors.red,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40.0),
+                        child: Container(
+                          height: 1,
+                          color: const Color.fromARGB(255, 240, 239, 239),
+                        ),
+                      ),
+                      SettingsItemLayout(
+                        icon: Icons.report,
+                        title: "Report problem",
+                        iconColor: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
