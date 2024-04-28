d1145d396e9a0db07f5ef427a124f4c4327153ba
Eugene Asiedu <uginasiedu079@gmail.com>
2024-04-28 02:28:36 +0200

    Library and permissions added for retriving contacts

--- android/app/src/main/kotlin/com/example/rapidlie/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/rapidlie/MainActivity.kt
@@ -3,4 +3,9 @@ package com.example.rapidlie
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        flutterEngine.plugins.add(PermissionHandlerPlugin())
+    }
 }
