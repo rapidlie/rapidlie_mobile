8a17abbc7b7ed714832da386dfa8d8e5424ef65d
Eugene Asiedu <uginasiedu079@gmail.com>
2024-05-28 20:23:25 +0200

    Added contacts and inviting contacts

--- lib/features/contacts/data/models/contact_details.dart
+++ lib/features/contacts/data/models/contact_details.dart
@@ -3,8 +3,15 @@ class ContactDetails {
   String? telephone;
   String? image;
   Status? status;
+  bool isSelected;
 
-  ContactDetails({required this.name, this.telephone, this.image, this.status});
+  ContactDetails({
+    required this.name,
+    this.telephone,
+    this.image,
+    this.status,
+    this.isSelected = false,
+  });
 }
 
 enum Status {
