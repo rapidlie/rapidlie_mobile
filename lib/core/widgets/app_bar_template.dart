bb92d929b3aeaecb806404c5492a40f1230d48d2
Eugene Asiedu <uginasiedu079@gmail.com>
2024-09-06 20:47:29 +0200

    a little modification to the appbar

--- lib/core/widgets/app_bar_template.dart
+++ lib/core/widgets/app_bar_template.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:rapidlie/core/constants/feature_contants.dart';
 
 class AppBarTemplate extends StatelessWidget {
@@ -27,7 +26,10 @@ class AppBarTemplate extends StatelessWidget {
               children: [
                 isSubPage
                     ? GestureDetector(
-                        onTap: () => Get.back(),
+                        onTap: () {
+                          print("Pressed");
+                          Navigator.pop(context);
+                        },
                         child: Padding(
                           padding: EdgeInsets.only(right: 10),
                           child: Icon(Icons.arrow_back),
