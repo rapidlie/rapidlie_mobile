bb243a6bc75496d69f1d0bf0d4747db2f0d7f661
Eugene Asiedu <uginasiedu079@gmail.com>
2024-10-13 00:00:36 +0200

    repository now handles all types of events

--- lib/features/events/repository/event_respository.dart
+++ lib/features/events/repository/event_respository.dart
@@ -7,7 +7,11 @@ import 'package:rapidlie/core/utils/shared_peferences_manager.dart';
 import 'package:rapidlie/features/events/models/event_model.dart';
 
 abstract class EventRepository {
-  Future<DataState<List<EventDataModel>>> getEventEntries();
+  Future<DataState<List<EventDataModel>>> getAllEvents();
+  Future<DataState<List<EventDataModel>>> getPrivateEvents();
+  Future<DataState<List<EventDataModel>>> getPublicEvents();
+  Future<DataState<List<EventDataModel>>> getEventsByCategory(
+      String categoryId);
 }
 
 class EventRepositoryImpl implements EventRepository {
@@ -15,12 +19,33 @@ class EventRepositoryImpl implements EventRepository {
 
   EventRepositoryImpl(this._dio);
 
+   @override
+  Future<DataState<List<EventDataModel>>> getAllEvents() async {
+    return _getEvents('$flockrAPIBaseUrl/events');
+  }
+
   @override
-  Future<DataState<List<EventDataModel>>> getEventEntries() async {
+  Future<DataState<List<EventDataModel>>> getPrivateEvents() async {
+    return _getEvents('$flockrAPIBaseUrl/events/private');
+  }
+
+  @override
+  Future<DataState<List<EventDataModel>>> getPublicEvents() async {
+    return _getEvents('$flockrAPIBaseUrl/events/public');
+  }
+
+  @override
+  Future<DataState<List<EventDataModel>>> getEventsByCategory(
+      String categoryId) async {
+    return _getEvents('$flockrAPIBaseUrl/events/category/$categoryId');
+  }
+
+  @override
+  Future<DataState<List<EventDataModel>>> _getEvents(String url) async {
     String bearerToken = await UserPreferences().getBearerToken();
     try {
       final response = await _dio.get(
-        '$flockrAPIBaseUrl/events/public',
+        url,
         options: Options(
           headers: {
             'Authorization': "Bearer " + bearerToken,
