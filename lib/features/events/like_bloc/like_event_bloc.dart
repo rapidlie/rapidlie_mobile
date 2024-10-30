bc3c117b8aaabe18f0a9c782cccf8193ef561393
Eugene Asiedu <uginasiedu079@gmail.com>
2024-10-30 03:43:35 +0100

    Like event bloc files added

---
+++ lib/features/events/like_bloc/like_event_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rapidlie/config/data_state.dart';
+import 'package:rapidlie/features/events/like_bloc/like_event_event.dart';
+import 'package:rapidlie/features/events/like_bloc/like_event_state.dart';
+import 'package:rapidlie/features/events/repository/like_event_repository.dart';
+
+class LikeEventBloc extends Bloc<LikeEventEvent, LikeEventState> {
+  final LikeEventRepository likeEventRepository;
+  LikeEventBloc({required this.likeEventRepository})
+      : super(InitialLikeEventState()) {
+    on<LikeToggled>(_onLikeEvent);
+  }
+
+  Future<void> _onLikeEvent(
+    LikeToggled event,
+    Emitter<LikeEventState> emit,
+  ) async {
+    emit(LikeEventLoading());
+    try {
+      final likeEventResponse =
+          await likeEventRepository.likeEvent(event.eventId);
+      if (likeEventResponse is DataSuccess) {
+        emit(LikeEventLoaded());
+      } else if (likeEventResponse is DataFailed) {
+        emit(LikeEventError(message: likeEventResponse.error.toString()));
+      }
+    } catch (e) {
+      emit(LikeEventError(message: e.toString()));
+    }
+  }
+}
