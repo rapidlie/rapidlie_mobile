80aab6b90b6a310f668d0511cc1b8d53fc45e204
Eugene Asiedu <uginasiedu079@gmail.com>
2024-10-31 17:27:26 +0100

    Like event repository file added

---
+++ lib/features/events/repository/like_event_repository.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:rapidlie/config/data_state.dart';
+import 'package:rapidlie/core/constants/strings.dart';
+import 'package:rapidlie/core/utils/shared_peferences_manager.dart';
+
+class LikeEventRepository {
+  final Dio dio;
+
+  LikeEventRepository({required this.dio});
+
+  Future<DataState<String>> likeEvent(String eventId) async {
+    String bearerToken = await UserPreferences().getBearerToken();
+
+    try {
+      final response = await dio.post(
+        '$flockrAPIBaseUrl/events/$eventId/like',
+        options: Options(
+          headers: {
+            'Authorization': "Bearer " + bearerToken,
+            'Accept': acceptString,
+          },
+        ),
+        data: {},
+      );
+
+      if (response.statusCode == HttpStatus.ok) {
+        return DataSuccess("success");
+      } else {
+        return DataFailed(
+          DioException(
+            error: response.statusMessage,
+            response: response,
+            type: DioExceptionType.badResponse,
+            requestOptions: response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      print(e);
+      return DataFailed(e);
+    }
+  }
+}
