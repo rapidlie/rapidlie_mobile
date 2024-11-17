3fa44b812c798d6cc46e788233cd7e3c913abf2b
Eugene Asiedu <uginasiedu079@gmail.com>
2024-11-17 18:14:50 +0100

    removed deprecated code

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -9,6 +9,8 @@ import 'app_localizations_de.dart';
 import 'app_localizations_en.dart';
 import 'app_localizations_fr.dart';
 
+// ignore_for_file: type=lint
+
 /// Callers can lookup localized strings with an instance of AppLocalizations
 /// returned by `AppLocalizations.of(context)`.
 ///
