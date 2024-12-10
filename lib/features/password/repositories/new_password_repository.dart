99fb0b37cb31251a069ee08d2174d29dcebb3d09
Eugene Asiedu <uginasiedu079@gmail.com>
2024-12-10 01:39:35 +0100

    repository files for password reset added

---
+++ lib/features/password/repositories/new_password_repository.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+import 'package:rapidlie/config/data_state.dart';
+import 'package:rapidlie/core/constants/strings.dart';
+
+class NewPasswordRepository {
+  final Dio dio;
+
+  NewPasswordRepository({required this.dio});
+
+  Future<DataState<String>> newPassword({
+    required String email,
+    required String otp,
+    required String password,
+  }) async {
+    try {
+      final response = await dio.post(
+        '$flockrAPIBaseUrl/password/reset',
+        options: Options(
+          headers: {
+            'Accept': acceptString,
+          },
+        ),
+        data: {
+          'email': email,
+          'otp': otp,
+          'password': password,
+        },
+      );
+
+      print(response.statusCode);
+      print(response.statusMessage);
+
+      if (response.statusCode == 200) {
+        return DataSuccess("success");
+      } else {
+        return DataFailed(DioException(
+          error: response.statusMessage,
+          response: response,
+          type: DioExceptionType.badResponse,
+          requestOptions: response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
---
+++ lib/features/password/repositories/request_repository.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:rapidlie/config/data_state.dart';
+import 'package:rapidlie/core/constants/strings.dart';
+
+class RequestRepository {
+  final Dio dio;
+
+  RequestRepository({required this.dio});
+
+  Future<DataState<String>> requestReset({
+    required String email,
+  }) async {
+    
+    try {
+      final response = await dio.post(
+        '$flockrAPIBaseUrl/password/request-otp',
+        options: Options(
+          headers: {
+            'Accept': acceptString,
+          },
+        ),
+        data: {
+          'email': email,
+        },
+      );
+
+      print(response.statusCode);
+      print(response.statusMessage);
+
+      if (response.statusCode == 200) {
+        return DataSuccess("success");
+      } else {
+        return DataFailed(DioException(
+          error: response.statusMessage,
+          response: response,
+          type: DioExceptionType.badResponse,
+          requestOptions: response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
