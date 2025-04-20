f6a57d931d2cec6f23c663cd156890bc924816c7
Eugene Asiedu <uginasiedu079@gmail.com>
2025-04-20 02:03:45 +0200

    added widget to launch Google map

---
+++ lib/features/events/presentation/pages/map_direction_launcher.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:rapidlie/core/constants/feature_constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MapDirectionLauncher extends StatefulWidget {
+  final LatLng targetLocation;
+
+  MapDirectionLauncher({required this.targetLocation});
+
+  @override
+  _MapDirectionLauncherState createState() => _MapDirectionLauncherState();
+}
+
+class _MapDirectionLauncherState extends State<MapDirectionLauncher> {
+  GoogleMapController? _controller;
+  LatLng _currentPosition = LatLng(0.0, 0.0); // Default, will be updated
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Location services are disabled.')));
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Location permissions are denied')));
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request them.')));
+      return;
+    }
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+      });
+    } catch (e) {
+      print("Error getting current location: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Could not get current location.')));
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller = controller;
+  }
+
+  Future<void> _openDirections() async {
+    print("Opening directions...");
+    print("Current Position: $_currentPosition");
+    print("Target Location: ${widget.targetLocation}");
+    if (_currentPosition.latitude == 0.0 && _currentPosition.longitude == 0.0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Waiting for current location...')));
+      return;
+    }
+
+    final String origin =
+        '${_currentPosition.latitude},${_currentPosition.longitude}';
+    final String destination =
+        '${widget.targetLocation.latitude},${widget.targetLocation.longitude}';
+    final String googleMapsUrl =
+        'https://www.google.com/maps/dir/?api=1&origin=$origin&destination=$destination&travelmode=driving';
+
+    if (await canLaunchUrl(Uri.parse(googleMapsUrl))) {
+      await launchUrl(Uri.parse(googleMapsUrl));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Could not open Google Maps.')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: Offset(0, 5), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius),
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              scrollGesturesEnabled: false,
+              zoomGesturesEnabled: false,
+              myLocationButtonEnabled: false,
+              initialCameraPosition: CameraPosition(
+                target: widget.targetLocation,
+                zoom: 16.0,
+              ),
+              mapType: MapType.normal,
+              markers: {
+                Marker(
+                  markerId: MarkerId("eventLocation"),
+                  position: widget.targetLocation,
+                  icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueCyan,
+                  ),
+                )
+              },
+            ),
+          ),
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(borderRadius),
+                onTap: () {
+                  print("Tapped on transparent layer");
+                  _openDirections();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/* onTap: () {
+          print("Tapped on map");
+          _openDirections();
+        }, */
