852e71a68482502825b58f8b18c81eb2bf21fc21
Eugene Asiedu <uginasiedu079@gmail.com>
2025-09-06 00:04:09 +0200

    renamed files

---
+++ lib/features/events/presentation/pages/create_event/step_1.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rapidlie/core/constants/custom_colors.dart';
+import 'package:rapidlie/core/constants/feature_constants.dart';
+import 'package:rapidlie/core/utils/app_snackbars.dart';
+import 'package:rapidlie/core/utils/image_picker.dart';
+import 'package:rapidlie/core/widgets/button_template.dart';
+import 'package:rapidlie/core/widgets/textfield_template.dart';
+import 'package:rapidlie/features/events/provider/create_event_provider.dart';
+import 'dart:io';
+
+class FirstSheetWidget extends StatefulWidget {
+  final PageController pageViewController;
+  final dynamic language;
+
+  FirstSheetWidget({
+    required this.pageViewController,
+    required this.language,
+  });
+
+  @override
+  State<FirstSheetWidget> createState() => _FirstSheetWidgetState();
+}
+
+class _FirstSheetWidgetState extends State<FirstSheetWidget> {
+  late TextEditingController titleController;
+  File? imageFile;
+
+  initState() {
+    super.initState();
+    titleController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.language.eventTitle + '*',
+            style: inter12CharcoalBlack400(),
+          ),
+          extraSmallHeight(),
+          TextFieldTemplate(
+            hintText: 'Title',
+            controller: titleController,
+            obscureText: false,
+            width: MediaQuery.of(context).size.width,
+            height: 50,
+            textInputType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+            enabled: true,
+            textFieldColor: Colors.white,
+          ),
+          smallHeight(),
+          Text(
+            widget.language.uploadFlyer + '*',
+            style: inter12CharcoalBlack400(),
+          ),
+          extraSmallHeight(),
+          GestureDetector(
+            onTap: () async {
+              File? file = await ImagePickerUtils.pickImageFromGallery(
+                  maxWidth: MediaQuery.of(context).size.width);
+              setState(() {
+                imageFile = file;
+              });
+            },
+            child: Container(
+              height: 160,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: CustomColors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: CustomColors.colorFromHex("#C6CDD3")),
+              ),
+              child: imageFile == null
+                  ? Icon(
+                      Icons.add_photo_alternate,
+                      size: 100,
+                      color: CustomColors.gray,
+                    )
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        child: Image.file(
+                          imageFile!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0),
+            child: ButtonTemplate(
+              buttonName: widget.language.next,
+              buttonWidth: MediaQuery.of(context).size.width,
+              buttonAction: () async {
+                if (titleController.text.isEmpty || imageFile == null) {
+                  AppSnackbars.showError(context, "All fields are required!");
+                  return;
+                } else {
+                  setState(() {});
+                  context.read<CreateEventProvider>().updateEvent(
+                        name: titleController.text,
+                        file: imageFile,
+                      );
+
+                  widget.pageViewController.nextPage(
+                    duration: Duration(milliseconds: 200),
+                    curve: Curves.easeIn,
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/events/presentation/pages/create_event/step_4.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:rapidlie/core/constants/custom_colors.dart';
+import 'package:rapidlie/core/constants/feature_constants.dart';
+import 'package:rapidlie/core/utils/app_snackbars.dart';
+import 'package:rapidlie/core/widgets/button_template.dart';
+import 'package:rapidlie/features/contacts/models/contact_details.dart';
+import 'package:rapidlie/features/contacts/presentation/pages/contact_list_screen.dart';
+import 'package:rapidlie/features/contacts/presentation/widgets/contact_list_item.dart';
+import 'package:rapidlie/features/events/blocs/create_bloc/create_event_bloc.dart';
+import 'package:rapidlie/features/events/provider/create_event_provider.dart';
+import 'package:rapidlie/features/file_upload/bloc/file_upload_bloc.dart';
+
+class FourthSheetContentWidget extends StatefulWidget {
+  final dynamic language;
+  final PageController pageViewController;
+
+  FourthSheetContentWidget({
+    required this.language,
+    required this.pageViewController,
+  });
+
+  @override
+  _FourthSheetContentWidgetState createState() =>
+      _FourthSheetContentWidgetState();
+}
+
+class _FourthSheetContentWidgetState extends State<FourthSheetContentWidget> {
+  List<ContactDetails> selectedContacts = [];
+
+  void _navigateAndSelectContacts() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ContactListScreen()),
+    );
+
+    if (result != null && result is List<ContactDetails>) {
+      setState(() {
+        selectedContacts = result;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    print(context.read<CreateEventProvider>().event.eventType.toString());
+
+    return BlocConsumer<FileUploadBloc, FileUploadState>(
+      listener: (context, state) {
+        if (state is FileUploadInitial) {
+          print("File upload initial");
+        } else if (state is FileUploadingState) {
+          print("File uploading...");
+        } else if (state is FileUploadSuccessState) {
+          debugPrint("File uploaded successfully");
+
+          context.read<CreateEventProvider>().updateEvent(
+                image: state.fileName,
+              );
+          //print(context.read<CreateEventProvider>().event);
+          BlocProvider.of<CreateEventBloc>(context).add(
+            SubmitCreateEventEvent(
+              image: state.fileName,
+              name: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .name!,
+              eventType:
+                  Provider.of<CreateEventProvider>(context, listen: false)
+                      .event
+                      .eventType!,
+              category: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .category!,
+              description:
+                  Provider.of<CreateEventProvider>(context, listen: false)
+                      .event
+                      .description!,
+              date: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .date!,
+              startTime:
+                  Provider.of<CreateEventProvider>(context, listen: false)
+                      .event
+                      .startTime!,
+              endTime: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .endTime!,
+              venue: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .venue!,
+              mapLocation:
+                  Provider.of<CreateEventProvider>(context, listen: false)
+                      .event
+                      .mapLocation!,
+              guests: Provider.of<CreateEventProvider>(context, listen: false)
+                  .event
+                  .guests!,
+            ),
+          );
+        } else if (state is FileUploadFailureState) {
+          AppSnackbars.showError(context, "File upload failed!");
+        } else {}
+      },
+      builder: (context, fileUploadState) {
+        return BlocConsumer<CreateEventBloc, CreateEventState>(
+          listener: (context, state) {
+            if (state is CreateEventSuccessful) {
+            } else if (state is CreateEventError) {
+              AppSnackbars.showError(context, "Error creating event!");
+            }
+          },
+          builder: (context, eventState) {
+            if (eventState is CreateEventLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (eventState is CreateEventSuccessful) {
+              widget.pageViewController.nextPage(
+                duration: Duration(milliseconds: 200),
+                curve: Curves.easeIn,
+              );
+            }
+            return Padding(
+              padding: const EdgeInsets.only(
+                  top: 0.0, left: 20, right: 20, bottom: 40),
+              child: selectedContacts.length == 0
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                _navigateAndSelectContacts();
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: CustomColors.black,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Icon(
+                                  Icons.add,
+                                  color: CustomColors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              widget.language.inviteFriends,
+                              style: poppins13black400(),
+                            ),
+                          ],
+                        ),
+                        context
+                                    .read<CreateEventProvider>()
+                                    .event
+                                    .eventType
+                                    .toString() ==
+                                "private"
+                            ? ButtonTemplate(
+                                buttonName: "PREVIOUS",
+                                buttonWidth: width,
+                                buttonAction: () {
+                                  widget.pageViewController.previousPage(
+                                    duration: Duration(milliseconds: 200),
+                                    curve: Curves.easeOut,
+                                  );
+                                },
+                              )
+                            : Row(
+                                children: [
+                                  Expanded(
+                                    flex: 3,
+                                    child: ButtonTemplate(
+                                      buttonName: "PREVIOUS",
+                                      buttonWidth:
+                                          MediaQuery.of(context).size.width,
+                                      borderColor: CustomColors.black,
+                                      buttonColor: CustomColors.white,
+                                      textColor: CustomColors.black,
+                                      buttonAction: () {
+                                        widget.pageViewController.previousPage(
+                                          duration: Duration(milliseconds: 200),
+                                          curve: Curves.easeOut,
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    flex: 6,
+                                    child: ButtonTemplate(
+                                      buttonName: "SKIP",
+                                      buttonWidth: width,
+                                      buttonAction: () {
+                                        context
+                                            .read<CreateEventProvider>()
+                                            .updateEvent(guests: []);
+                                        BlocProvider.of<FileUploadBloc>(context)
+                                            .add(
+                                          FileUploadEvent(
+                                            file: Provider.of<
+                                                        CreateEventProvider>(
+                                                    context,
+                                                    listen: false)
+                                                .fileUpload
+                                                .file!,
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                        SizedBox(),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: selectedContacts.length,
+                          itemBuilder: (context, index) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ContactListItem(
+                                  contactName: selectedContacts[index].name,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedContacts.removeAt(index);
+                                    });
+                                  },
+                                  child: Icon(
+                                    Icons.close,
+                                    color: Colors.grey,
+                                    size: 20,
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: ButtonTemplate(
+                                buttonName: "+",
+                                buttonWidth: width,
+                                buttonAction: () {
+                                  _navigateAndSelectContacts();
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            Flexible(
+                              flex: 8,
+                              child: ButtonTemplate(
+                                buttonName: "Finish",
+                                buttonWidth: width,
+                                buttonAction: () {
+                                  context
+                                      .read<CreateEventProvider>()
+                                      .updateEvent(
+                                        guests: selectedContacts
+                                            .map(
+                                                (contact) => contact.telephone!)
+                                            .toList(),
+                                      );
+                                  BlocProvider.of<FileUploadBloc>(context).add(
+                                    FileUploadEvent(
+                                      file: Provider.of<CreateEventProvider>(
+                                              context,
+                                              listen: false)
+                                          .fileUpload
+                                          .file!,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
